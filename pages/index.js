9616b1ef7960138f8b94220b3be1a59ceea8888e
SergeyZu <vechniystudent19@yandex.ru>
2023-01-27 06:59:00 +0700

    Отменил последние изменения. Сайт заработал

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { buttonCloseList, profileTitleElement, profileSubtitleElement, popupProfileElement, formProfileElement, formNameElement, formAboutElement, profileEditButtonElement, popupCardElement, buttonAddCardElement, formCardElement, cardTitle, link, config, popupTitle, popupImage, popupImageElement, cardsContainer } from '../utils/constants.js';
 
-// import { openPopup, closePopup, closePopupByClickOnOverlay, cardFormSubmitHandler, rendererCard } from '../utils/utils.js';
+import { openPopup, closePopup, closePopupByClickOnOverlay, cardFormSubmitHandler, rendererCard } from '../utils/utils.js';
 
-import { cardFormSubmitHandler, rendererCard } from '../utils/utils.js';
+// import { cardFormSubmitHandler, rendererCard } from '../utils/utils.js';
 
 import { cardsDataElement } from '../data/cardsData.js';
 
@@ -10,16 +10,16 @@ import Card from '../components/Card.js';
 
 import Section from '../components/Section.js'
 
-import Popup from '../components/Popup.js'
+// import Popup from '../components/Popup.js'
 
 import FormValidator from '../components/FormValidator.js';
 
-// // Универсальная функция закрытия попапа
-// buttonCloseList.forEach(btn => {
-//     const popup = btn.closest('.popup');
-//     popup.addEventListener('mousedown', closePopupByClickOnOverlay);
-//     btn.addEventListener('click', () => closePopup(popup)); 
-// })
+// Универсальная функция закрытия попапа
+buttonCloseList.forEach(btn => {
+    const popup = btn.closest('.popup');
+    popup.addEventListener('mousedown', closePopupByClickOnOverlay);
+    btn.addEventListener('click', () => closePopup(popup)); 
+})
 
 
 // Функция открытия попапа Профиль
@@ -29,12 +29,12 @@ function openProfilePopup () {
     formAboutElement.value = profileSubtitleElement.textContent;
 };
 
-// function profileFormSubmitHandler (evt) {
-//     evt.preventDefault();
-//     profileTitleElement.textContent = formNameElement.value;
-//     profileSubtitleElement.textContent = formAboutElement.value;
-//     closePopup(popupProfileElement);
-// };
+function profileFormSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitleElement.textContent = formNameElement.value;
+    profileSubtitleElement.textContent = formAboutElement.value;
+    closePopup(popupProfileElement);
+};
 
 function openCardPopup () {
     popupCardValidator.resetValidation();
@@ -74,9 +74,9 @@ const cardSection = new Section({
 );
 
 
-const popup = new Popup('.popup');
+// const popup = new Popup('.popup');
 
-const popupWithImage = new PopupWithImage(popupImageElement, cardsDataElement)
+// const popupWithImage = new PopupWithImage(popupImageElement, cardsDataElement)
 
 
 
--- utils/utils.js
+++ utils/utils.js
@@ -2,32 +2,32 @@ import Card from '../components/Card.js';
 import { popupTitle, popupImage, popupImageElement, cardsContainer, cardTitle, link, popupCardElement } from '../utils/constants.js';
 import { cardsDataElement } from '../data/cardsData.js';
 
-// // Функция отображения попапа
-// function openPopup(popupName) {
-//     popupName.classList.add('popup_opened');
-//     document.addEventListener('keyup', closePopupByEsc);
-// }
+// Функция отображения попапа
+function openPopup(popupName) {
+    popupName.classList.add('popup_opened');
+    document.addEventListener('keyup', closePopupByEsc);
+}
 
-// // Функция закрытия попапа 
-// function closePopup(popupName) {
-//     popupName.classList.remove('popup_opened');
-//     document.removeEventListener('keyup', closePopupByEsc);
-// }
+// Функция закрытия попапа 
+function closePopup(popupName) {
+    popupName.classList.remove('popup_opened');
+    document.removeEventListener('keyup', closePopupByEsc);
+}
 
-// // Функция закрытия попапа по клику на оверлей
-// function closePopupByClickOnOverlay (event) {
-//     if (event.target === event.currentTarget) {
-//         closePopup(event.target);
-//     }
-// }
+// Функция закрытия попапа по клику на оверлей
+function closePopupByClickOnOverlay (event) {
+    if (event.target === event.currentTarget) {
+        closePopup(event.target);
+    }
+}
 
-// // Функция закрытия попапа через ESC
-// function closePopupByEsc (event) {
-//     if (event.key === 'Escape' ) {
-//         const popup = document.querySelector('.popup_opened');
-//         closePopup(popup);
-//     }
-// }
+// Функция закрытия попапа через ESC
+function closePopupByEsc (event) {
+    if (event.key === 'Escape' ) {
+        const popup = document.querySelector('.popup_opened');
+        closePopup(popup);
+    }
+}
 
 function cardFormSubmitHandler(event) {
     event.preventDefault();
@@ -41,12 +41,12 @@ cardsDataElement.forEach((item) => {
 });
 
 
-// function viewImageHandler(name, link) {
-//     popupTitle.textContent = name;
-//     popupImage.src = link;
-//     popupImage.alt = name;
-//     openPopup(popupImageElement);
-// }
+function viewImageHandler(name, link) {
+    popupTitle.textContent = name;
+    popupImage.src = link;
+    popupImage.alt = name;
+    openPopup(popupImageElement);
+}
 
 function createCard(item) {
     const card = new Card(item, '#card-template', viewImageHandler );
@@ -59,5 +59,5 @@ function rendererCard(item) {
     cardsContainer.prepend(cardItem);
 };
 
-// export { openPopup, closePopup, closePopupByClickOnOverlay, closePopupByEsc, rendererCard, createCard, cardFormSubmitHandler };
-export { rendererCard, createCard, cardFormSubmitHandler };
+export { openPopup, closePopup, closePopupByClickOnOverlay, closePopupByEsc, rendererCard, createCard, cardFormSubmitHandler };
+// export { rendererCard, createCard, cardFormSubmitHandler };
