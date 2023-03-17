0d45a1501592fcf7fb8b4e2fc828c0fc53f0b5a5
SergeyZu <vechniystudent19@yandex.ru>
2023-03-17 11:37:25 +0700

    Настроил отправку запросов на сервер через функцию _request()

--- src/components/Api.js
+++ src/components/Api.js
@@ -11,7 +11,7 @@ export default class Api {
         return Promise.reject(`Ошибка: ${res.status}`);
     }
     // Альтернативный вариант записи вместо handleResponse:
-        // .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
+    // .then(res => res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`));
     
     
     _request(url, options) {
@@ -20,45 +20,35 @@ export default class Api {
 
 
     // Запрос на получение данных пользователя
-    getUserData () {
-        
-    // Попробовал реализовать запросы через _request(), - не смог разобраться, почему сервер вместо данных пользователя возвращает undefined/
-    
-        // this._request(`${this._baseUrl}/users/me`, 
-        //     {headers: this._headers}
-        // )
+    getUserData () {    
+        return this._request(`${this._baseUrl}/users/me`, 
+            {headers: this._headers}
+        )
 
-        return fetch(`${this._baseUrl}/users/me`, {
-            headers: this._headers
-        })
-        .then(this._handleResponse); 
-        
     }
 
 
     // Запрос на изменение данных пользователя
     setUserData (inputValues) {
-        return fetch(`${this._baseUrl}/users/me`, {
+        return this._request(`${this._baseUrl}/users/me`, {
             method: 'PATCH', 
             headers: this._headers,
             body: JSON.stringify(inputValues)
         })
-        .then(this._handleResponse); 
     }
     
 
     // Запрос на получение массива всех карточек
     getInitialCards () {
-        return fetch(`${this._baseUrl}/cards`, {
+        return this._request(`${this._baseUrl}/cards`, {
             headers: this._headers
         })
-        .then(this._handleResponse); 
     }
 
     
     // Запрос на добавление карточки
     addCard (data) {
-        return fetch(`${this._baseUrl}/cards`, {
+        return this._request(`${this._baseUrl}/cards`, {
             method: 'POST',
             headers: this._headers,
             body: JSON.stringify({
@@ -66,50 +56,45 @@ export default class Api {
                 link: data.link
             })
         })
-        .then(this._handleResponse); 
     }
 
 
     // Запрос на удаление карточки
     deleteCard (cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}`, {
+        return this._request(`${this._baseUrl}/cards/${cardId}`, {
             method: 'DELETE',
             headers: this._headers
         })
-        .then(this._handleResponse); 
     }
 
 
     // Запрос на постановку лайка
     setLike (cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        return this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
             method: 'PUT',
             headers: this._headers
         })
-        .then(this._handleResponse); 
     }
 
 
     // Запрос на снятие лайка
     removeLike (cardId) {
-        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+        return this._request(`${this._baseUrl}/cards/${cardId}/likes`, {
             method: 'DELETE',
             headers: this._headers
         })
-        .then(this._handleResponse); 
     }
 
 
     // Запрос на обновление аватара
     changeAvatar (avatar) {
-        return fetch(`${this._baseUrl}/users/me/avatar`, {
+        return this._request(`${this._baseUrl}/users/me/avatar`, {
             method: 'PATCH',
             headers: this._headers,
             body: JSON.stringify({
                 avatar: avatar
             })        
         })
-        .then(this._handleResponse); 
     }
 
 }
